899f79ad86514216f450af19be62fbb76ac0f2f4
Leonardo Ascione <me@leonardfactory.com>
2014-12-14 19:36:45 +0100

    Login with Parse.com

--- src/app/data/parseService.js
+++ src/app/data/parseService.js
@@ -6,6 +6,9 @@ angular
             init: function () {
                 // Our connection to parse.com.
                 Parse.initialize("iWpIHpzgt0smKYu82KpebMVwoWtNIHNPb1NpAKU9", "Z5qq7rp54YLvw9RGpt5X3jKJ7WLwurx3UzOBJVuY");
+            },
+            logged: function () {
+                return Parse.User.current() !== null;
             }
         }
     });
---
+++ src/app/login/LoginCtrl.js
@@ -0,0 +1,37 @@
+angular
+    .module('cinApp')
+    .controller('LoginCtrl', function ($scope, parseService) 
+    {
+        var _this = this;
+        
+        this.logged = parseService.logged();
+        
+        this.user = {
+            username: '',
+            password: ''
+        }
+        
+        this.alert = null;
+        
+        this.closeAlert = function () 
+        {
+            _this.alert = null;
+        }
+        
+        this.login = function () 
+        {
+            _this.alert = null;
+            
+            Parse.User
+                .logIn(_this.user.username, _this.user.password)
+                .then(function (user) 
+                {
+                    _this.logged = parseService.logged();
+                },
+                function (error) 
+                {
+                    _this.logged = false;
+                    _this.alert = 'Error while logging: ' + error;
+                });
+        }
+    });
