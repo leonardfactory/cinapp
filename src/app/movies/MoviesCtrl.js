e09609e33cab89dcc249a29a5f57bd15d320aed8
Leonardo Ascione <me@leonardfactory.com>
2014-12-14 19:37:05 +0100

    Watched movies adder & list with Parse.com

--- src/app/movies/MoviesCtrl.js
+++ src/app/movies/MoviesCtrl.js
@@ -1,13 +1,39 @@
 angular
     .module('cinApp')
-    .controller('MoviesCtrl', ['$scope', function ($scope) 
+    .controller('MoviesCtrl', function ($scope, User, Movie, WatchedCollection) 
     {
         var _this = this;
         
         this.inlineMovie = {};
         
-        this.movies = [
-            { name: 'Big Hero 6' },
-            { name: 'Magic in the moonlight' }
-        ];
-    }]);
+        this.loading = true;
+        
+        this.movies = new WatchedCollection();
+        this.movies.fetch().then(function () {
+            _this.loading = false; 
+        });
+        
+        this.alerts = [];
+        
+        this.closeAlert = function(index) 
+        {
+            _this.alerts.splice(index, 1);
+        };
+        
+        this.addMovie = function () 
+        {
+            var movie = new Movie();
+            movie.setName(_this.inlineMovie.name);
+            movie.setDirector(_this.inlineMovie.director);
+            movie.setYear(_this.inlineMovie.year);
+            
+            _this.movies.addMovie(movie).then(function () {
+                User.current().relation('watched').add(movie);
+                
+                _this.alerts.push({
+                    message: 'Added movie `' + movie.getName() + '`',
+                    type: 'success'
+                });
+            });
+        };
+    });
