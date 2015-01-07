bb30507503b192e2d147b29d4b0048789ef1d247
Leonardo Ascione <me@leonardfactory.com>
2015-01-07 20:28:34 +0100

    Style fixes to movies list

--- src/app/search/SearchCtrl.js
+++ src/app/search/SearchCtrl.js
@@ -1,26 +1,81 @@
 angular
     .module('cinApp')
-    .controller('SearchCtrl', function (tmdbService) 
+    .controller('SearchCtrl', function ($q, $scope, $timeout, tmdbService, loaderService) 
     {
         var _this = this;
         
         this.query = '';
         this.queryError = false;
         
-        this.results = [];
+        this.tempResults    = [];
+        this.results        = [];
         
+        /**
+         * Search for a query with TMDB APIs, showing only meaningful results.
+         */
         this.search = function () 
         {
             if(_this.query !== '') 
             {
+                loaderService.start();
+                
                 _this.queryError = false;
+                _this.results = [];
                 tmdbService.api
-                    .search.getMovie({ query: _this.query })
+                    .search.getMovie({ query: _this.query, language: 'it' })
                     .then(function (data) 
                     {
-                        console.log('Typeof data: ' + typeof data)
-                        console.log(data);
-                        _this.results = data.results;
+                        // Show max 10 results
+                        _this.tempResults = data.results;
+                        if(_this.tempResults.length > 10) {
+                            _this.tempResults = data.results.splice(0, 10);
+                        }
+                        
+                        // Start processing results with a delay.
+                        // Since we have a strict request/minute limit imposed by TMDB, we can use
+                        // this time to show a nice animation while fetching for successive results.
+                        var promises = [];
+                        var i = 0;
+                        angular.forEach(_this.tempResults, function (tempResult) 
+                        {
+                            var result;
+                            var promise = $timeout(function () {
+                                    return tmdbService.api.movies.getById({ id: tempResult.id, language: 'it' });
+                                }, 500 * i++)
+                                .then(function (movie) {
+                                    result = movie;
+                                    return tmdbService.api.movies.getCredits({ id: tempResult.id });
+                                })
+                                .then(function (credits) {
+                                    var director;
+                                    try {
+                                        director = _.find(credits.crew, function (crewMember) {
+                                            return crewMember.job === "Director";
+                                        }).name;
+                                    }
+                                    catch (e) {
+                                        director = null;
+                                    }
+                                    result.director = director;
+                                    
+                                    // Filter only meaningful reasults
+                                    if(result.director === null) return;
+                                    if(!result.poster_path) return;
+                                    
+                                    _this.results.push(result);
+                                });
+                            
+                            promises.push(promise);
+                        });
+                        
+                        return $q.all(promises);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    })
+                    .finally(function () {
+                        loaderService.done();
+                        $scope.$apply();
                     });
             }
             else
