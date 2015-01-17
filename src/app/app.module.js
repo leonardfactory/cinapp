c0f3a52cafd5bf0c9de5925e221c2161ec7b22ec
Leonardo Ascione <me@leonardfactory.com>
2015-01-17 21:11:03 +0100

    Removed unused dependencies, version bump

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -2,13 +2,12 @@
  * Cinapp entry point.
  * Here we define the ngApp while initializing some useful stuff.
  */
-var models = angular.module('cinApp.models', ['parse-extend']);
+var models = angular.module('cinApp.models', ['ngParse']);
 var app = angular.module('cinApp', ['cinApp.models', 'lfExtensions', 'angular-modal',
-                                    'ngAnimate', 'ngParse', 'lfEffects', 'ui.router', 'templates', 'truncate']);
+                                    'ngAnimate', 'lfEffects', 'ui.router', 'templates', 'truncate']);
 var templates = angular.module('templates', []);
 
-app.run(function (NgParse, User, parseService, tmdbService) 
+app.run(function (NgParse, User, tmdbService) 
 {
-    parseService.init();
     NgParse.initialize('iWpIHpzgt0smKYu82KpebMVwoWtNIHNPb1NpAKU9', 'pndsi4DCFDZBzB0YQK4m2D8wwLr779mdhkggC30m');
 });
